5068de66d0833c07d03429299223dc953896c1c2
miwa <dskipineapple@gmail.com>
2024-09-20 22:29:29 +0900

    MVCを使ってArticleを表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
-    render 'home/index'
+    @article = Article.first
+  end
+
+  def about
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 2024_09_20_122918) do
 
   create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
